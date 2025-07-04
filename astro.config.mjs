1ac20bbe3931cb33e5e2fd09d9ae6518143d79a6
楊竣宇 <yangjunyu@yangjunyudeMacBook-Air.local>
2025-07-04 15:22:05 +0800

    fix seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hongyao.com.tw/',
   base: '/HongYao/',
   outDir: 'docs',
+  integrations: [sitemap()],
   vite: {
     css: {
       preprocessorOptions: {
