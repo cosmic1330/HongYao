81965221065979d9a806bbcbf7aaf6b141b4daee
楊竣宇 <yangjunyu@yangjunyudeMacBook-Air.local>
2025-07-04 13:41:24 +0800

    fix: image path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cosmic1330.github.io',
+  base: '/HongYao',
   outDir: 'docs',
   vite: {
     css: {
