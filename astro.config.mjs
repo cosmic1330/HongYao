af94d54f0661ecd25cb2eaa4f20dd2db634966e9
楊竣宇 <yangjunyu@yangjunyudeMacBook-Air.local>
2025-07-04 14:05:23 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/HongYao',
+  base: '/',
   outDir: 'docs',
   vite: {
     css: {
